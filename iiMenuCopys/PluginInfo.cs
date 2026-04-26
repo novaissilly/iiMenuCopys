475ae7cb2764971d2af6948b2b1ef1678479951a
Novaafr <idk777800111@gmail.com>
2026-04-26 22:51:37 +0100

    2.7.7

--- iiMenuCopys/PluginInfo.cs
+++ iiMenuCopys/PluginInfo.cs
@@ -10,7 +10,7 @@ namespace iiMenu
         public const string GUID = "org.nova.gorillatag.iimenucopys";
         public static string Name = "ii's Stupid Menu";
         public const string Description = "Created by @goldentrophy with love <3 x Nova for porting";
-        public const string Version = "2.7.6";
+        public const string Version = "2.7.7";
         // https://pastebin.com/raw/yApU6qHZ - version link
     }
 }
